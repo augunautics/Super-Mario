ad1efcb4a5126849ea01a26728ababf2eb5fbb3b
augunautics <thomas.a.augustine@gmail.com>
2024-09-01 08:35:24 -0700

    scale 3

--- Constants.js
+++ Constants.js
@@ -6,7 +6,7 @@
 export default class Constants {
     constructor({}) {}
 
-    static aspectRatio = 2;
+    static aspectRatio = 3;
     static spriteSize = 16;
     static borderSize = 16;
     static startLocation = 200;
